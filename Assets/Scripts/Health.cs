af72bea106c61612bccdac037d6b52dcef396836
JasonMadness <tobc_madness@mail.ru>
2026-09-10 19:18:24 +0300

    Create Health.cs

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,40 @@
+using System;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField] private int _max = 100;
+
+    private int _current;
+
+    public event Action DamageTaken;
+    public event Action Died;
+
+    public int Current => _current;
+    public int Max => _max;
+
+    private void Awake()
+    {
+        _current = _max;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        _current -= damage;  
+        DamageTaken?.Invoke();
+
+        if (_current <= 0)
+        {
+            _current = 0;
+            Died?.Invoke();
+        }
+    }
+
+    public void Increase(int amount)
+    {
+        _current += amount;
+
+        if (_current > _max)
+            _current = _max;
+    }
+}
