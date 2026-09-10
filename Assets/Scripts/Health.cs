c9b7102e92888123d82cab62178fcda23dd6e0ce
JasonMadness <tobc_madness@mail.ru>
2026-09-10 19:59:47 +0300

    Update Health.cs

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -17,6 +17,11 @@ public class Health : MonoBehaviour
         _current = _max;
     }
 
+    private void Start()
+    {
+        ValueChanged?.Invoke(_current);
+    }
+
     public void Decrease(int damage)
     {
         _current -= damage;
