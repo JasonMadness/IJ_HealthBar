d6878b388b61697cb59983f826bc232a2a4f2f3a
JasonMadness <tobc_madness@mail.ru>
2026-09-10 21:21:29 +0300

    Update RawBar.cs

--- Assets/Scripts/RawBar.cs
+++ Assets/Scripts/RawBar.cs
@@ -1,6 +1,12 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class RawBar : Bar
 {
-    
+    [SerializeField] private Slider _slider;
+
+    protected override void UpdateView()
+    {
+        _slider.value = (float)CurrentValue / MaxValue;
+    }
 }
