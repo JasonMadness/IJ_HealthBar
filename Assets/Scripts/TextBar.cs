373d7d2ef808de124bd006afec1ad0ac980ebb8a
JasonMadness <tobc_madness@mail.ru>
2026-09-10 19:57:40 +0300

    Update TextBar.cs

--- Assets/Scripts/TextBar.cs
+++ Assets/Scripts/TextBar.cs
@@ -4,5 +4,10 @@ using UnityEngine;
 
 public class TextBar : Bar
 {
-    
+    [SerializeField] private TMPro.TextMeshPro _text;
+
+    protected override void UpdateView()
+    {
+        _text.text = $"{CurrentValue}/{MaxValue}";
+    }
 }
